c6b0dd355972334676c88241dd51b80067bcc004
Sara Alqallaf <202107125@stu.uob.edu.bh>
2025-09-02 10:06:51 +0300

    added a USER model with roles and cancel the Designer model

--- main_app/models.py
+++ main_app/models.py
@@ -1,25 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 
-class Designer(models.Model):
-    first_name = models.CharField(max_length=80)
-    last_name = models.CharField(max_length=80)
-    years_of_experience = models.IntegerField()
+# class Designer(models.Model):
+#     first_name = models.CharField(max_length=80)
+#     last_name = models.CharField(max_length=80)
+#     years_of_experience = models.IntegerField()
     
-    class Meta:
-        db_table = 'designers'
+#     class Meta:
+#         db_table = 'designers'
         
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name}"
+
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "admin", "Admin" # value saved in db, value shown to the user
+        DESIGNER = "designer", "Designer"
+        CUSTOMER = "customer", "Customer"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.CUSTOMER,
+        # null=True
+    )
+    
     
 class Project(models.Model):
     name = models.CharField(max_length=150)
     date = models.DateField()
     description = models.TextField(null = True)
     pictures = models.ImageField(upload_to='projects-pictures/', null=True)
-    designer = models.ForeignKey(Designer , on_delete=models.CASCADE , related_name='projects')
+    designer = models.ForeignKey(User.Role.DESIGNER , on_delete=models.CASCADE , related_name='projects')
     # category = models.CharField()
     
     class Meta:
